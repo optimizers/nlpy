2b45bee50f56285613cf09cc5cc2d299ca4d3e60
Dominique Orban <dominique.orban@gmail.com>
2010-08-05 14:17:52 +0200

    Made timing module platform dependent.
    The 'resource' module is not available on non-Unix-like systems.

--- nlpy/tools/timing.py
+++ nlpy/tools/timing.py
@@ -1,5 +1,12 @@
-# $Id:$
-import resource
+# Platform-dependent time measurement.
 
-def cputime():
-	return resource.getrusage(resource.RUSAGE_SELF)[0]
+try:
+    # Use resource module if available.
+    import resource
+    def cputime():
+        return resource.getrusage(resource.RUSAGE_SELF)[0]
+except:
+    # Fall back on time.clock().
+    import time
+    def cputime():
+        return time.clock()
