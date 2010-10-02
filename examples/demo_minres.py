4c7386cfaec1ef30f3080fd3e698f850a4920576
Dominique Orban <dominique.orban@gmail.com>
2010-10-02 18:15:53 -0400

    Converted demo_minres to use linear operators.

--- examples/demo_minres.py
+++ examples/demo_minres.py
@@ -3,36 +3,26 @@ Minres demo.
 """
 
 from nlpy.krylov import Minres
+from nlpy.krylov.linop import SimpleLinearOperator
 from nlpy.precon import DiagonalPreconditioner
-from pysparse import spmatrix
 import numpy as np
 
-def diagA(n):
-    "Creates A = diag(1,2,...,n) and b = [1, 1, ..., 1]"
-    A = spmatrix.ll_mat_sym(n,n)
-    for i in range(n): A[i,i] = i+1
-    b = np.ones(n)
-    return (A,b)
-
-def test1(A, b, s=0.0, M=None):
-    "Perform Minres solve with specified arguments"
-    K = Minres(A, shift=s, precon=M)
-    K.solve(b)
-    print 'Solution: ' ; print K.x
-    return
-
 if __name__ == '__main__':
+
     # Set printing standards for arrays
     np.set_printoptions(precision=3, linewidth=80, threshold=10, edgeitems=3)
 
-    n = 10
-    (A,b) = diagA(n)
+    # Create A = diag(1,2,...,n) and b = [1, 1, ..., 1].
+    n = 10  # Must have n > 4.
+    A = SimpleLinearOperator(n, n, lambda v: np.arange(1,n+1)*v, symmetric=True)
+    b = np.ones(n)
     
-    #test1(A, b)               # Solves Ax = b
-    #test1(A, b, s=2.0)        # Solves (A-2I)x = b
+    M = DiagonalPreconditioner(np.arange(1,n+1))
+    for i in range(1,4): M[i] = 1.0
 
-    goodM = DiagonalPreconditioner(1+np.arange(n))
-    for i in range(1,4): goodM[i] = 1.0
+    K = Minres(A)
+    K.solve(b, precon=M, show=True) # Solves Ax = b with preconditioner M.
+    print 'Solution: ' ; print K.x
 
-    test1(A, b, M=goodM.precon)      # Solves Ax = b with good preconditioner M
-    #test1(A, b, s=2.0, M=goodM.precon)  # Solves (A-2I)x = b with good precon M
+    K.solve(b, shift=2.0, precon=M, show=True) # Solves (A-2I)x = b.
+    print 'Solution: ' ; print K.x
