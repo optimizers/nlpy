5cf1186df04b0538315d9b97ab5f4578695824be
Dominique Orban <dominique.orban@gmail.com>
2011-01-18 08:43:45 -0500

    Fix indentation to build scaling module.

--- nlpy/linalg/setup.py
+++ nlpy/linalg/setup.py
@@ -241,7 +241,7 @@ def configuration(parent_package='',top_path=None):
             f2py_options=f2py_options,
         )
 
-        config.add_subpackage('scaling')
+    config.add_subpackage('scaling')
 
     config.make_config_py()
     return config
