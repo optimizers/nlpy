53f6a77121d642d81e92ff7cbe861137803d0f62
Dominique Orban <dominique.orban@gmail.com>
2011-03-11 16:12:15 -0500

    Add sparse Jacobian to ADOL-C models.

--- nlpy/model/adolcmodel.py
+++ nlpy/model/adolcmodel.py
@@ -2,10 +2,12 @@ from nlpy.model import NLPModel
 import adolc
 import numpy as np
 
-
+has_colpack = False
 
 class AdolcModel(NLPModel):
 
+    # Count the number of instances of this class to generate
+    # non-conflicting tape ids. Must be a mutable type.
     __NUM_INSTANCES = [-1]
 
     def __init__(self, n=0, m=0, name='Generic', **kwargs):
@@ -14,9 +16,10 @@ class AdolcModel(NLPModel):
 
         # Trace objective and constraint functions.
         self._obj_trace_id = None
-        self._con_trace_id = None
         self._trace_obj(self.x0)
-        self._trace_con(self.x0)
+
+        self._con_trace_id = None
+        if self.m > 0: self._trace_con(self.x0)
 
         self.first_sparse_hess_eval = True
         self.first_sparse_jac_eval  = True
@@ -41,7 +44,7 @@ class AdolcModel(NLPModel):
 
         if self._obj_trace_id is None:
 
-            print 'Tracing objective...'
+            #print 'Tracing objective...'
             self._obj_trace_id = self._get_trace_id()
             adolc.trace_on(self._obj_trace_id)
             x = adolc.adouble(x)
@@ -53,9 +56,9 @@ class AdolcModel(NLPModel):
 
     def _trace_con(self, x):
 
-        if self._con_trace_id is None:
+        if self._con_trace_id is None and self.m > 0:
 
-            print 'Tracing constraints...'
+            #print 'Tracing constraints...'
             self._con_trace_id = self._get_trace_id() + 1
             adolc.trace_on(self._con_trace_id)
             x = adolc.adouble(x)
@@ -82,6 +85,13 @@ class AdolcModel(NLPModel):
 
     def hess(self, x, z, **kwargs):
         "Return the Hessian of the objective at x."
+        if has_colpack:
+            return self.sparse_hess(x, z, **kwargs)
+        return self.dense_hess(x, z, **kwargs)
+
+
+    def dense_hess(self, x, z, **kwargs):
+        "Return the Hessian of the objective at x in dense format."
         return adolc.hessian(self._obj_trace_id, x)
 
 
@@ -91,11 +101,14 @@ class AdolcModel(NLPModel):
 
 
     def sparse_hess(self, x, z, **kwargs):
-        "Return the Hessian of the objective at x."
+        "Return the Hessian of the objective at x in sparse format."
         options = np.zeros(2,dtype=int)
         if self.first_sparse_hess_eval:
-            nnz, rind, cind, values =  adolc.colpack.sparse_hess_no_repeat(self._obj_trace_id, x, options=options)
-            self.hess_nnz  = nnz
+            nnz, rind, cind, values =  \
+                    adolc.colpack.sparse_hess_no_repeat(self._obj_trace_id,
+                                                        x,
+                                                        options=options)
+            self.nnzH  = nnz
             self.hess_rind = rind
             self.hess_cind = cind
             self.hess_values = values
@@ -103,7 +116,11 @@ class AdolcModel(NLPModel):
             return rind, cind, values
 
         else:
-            return adolc.colpack.sparse_hess_repeat(self._obj_trace_id, x, self.hess_rind, self.hess_cind, self.hess_values)
+            return adolc.colpack.sparse_hess_repeat(self._obj_trace_id,
+                                                    x,
+                                                    self.hess_rind,
+                                                    self.hess_cind,
+                                                    self.hess_values)
 
 
     def _adolc_cons(self, x, **kwargs):
@@ -113,12 +130,44 @@ class AdolcModel(NLPModel):
 
     def jac(self, x, **kwargs):
         "Return constraints Jacobian at x."
+        if has_colpack:
+            return self.sparse_jac(x, **kwargs)
+        return self.dense_jac(x, **kwargs)
+
+
+    def dense_jac(self, x, **kwargs):
+        "Return constraints Jacobian at x in dense format."
         return self._adolc_jac(x, **kwargs)
 
 
     def _adolc_jac(self, x, **kwargs):
         "Evaluate the constraints Jacobian from the ADOL-C tape."
         return adolc.jacobian(self._con_trace_id, x)
+
+
+    def sparse_jac(self, x, **kwargs):
+        "Return constraints Jacobian at x in sparse format."
+        [nnz, rind, cind, values] =sparse_jac_no_repeat(tape_tag, x, options)
+        options = np.zeros(4,dtype=int)
+        if self.first_sparse_jac_eval:
+            nnz, rind, cind, values =  \
+                    adolc.colpack.sparse_jac_no_repeat(self._con_trace_id,
+                                                       x,
+                                                       options=options)
+            self.nnzJ  = nnz
+            self.jac_rind = rind
+            self.jac_cind = cind
+            self.jac_values = values
+            self.first_sparse_jac_eval = False
+            return rind, cind, values
+
+        else:
+            return adolc.colpack.sparse_jac_repeat(self._jac_trace_id,
+                                                   x,
+                                                   self.jac_rind,
+                                                   self.jac_cind,
+                                                   self.jac_values)
+
 
 
     def jac_vec(self, x, v, **kwargs):
@@ -159,8 +208,8 @@ if __name__ == '__main__':
             return (1 + x[0]**2)**2 + x[1]**2 - 4
 
 
-    nvar = 10
-    #rosenbrock = AdolcRosenbrock(n=nvar, name='Rosenbrock', x0=-np.ones(nvar))
+    nvar = 5
+    rosenbrock = AdolcRosenbrock(n=nvar, name='Rosenbrock', x0=-np.ones(nvar))
     hs7 = AdolcHS7(n=2, m=1, name='HS7', x0=2*np.ones(2))
 
     nlp = hs7
@@ -168,20 +217,22 @@ if __name__ == '__main__':
     g = nlp.grad(nlp.x0)
     H = nlp.hess(nlp.x0, nlp.x0)
     #H_sparse = nlp.sparse_hess(nlp.x0, nlp.x0)
-    c = nlp.cons(nlp.x0)
-    J = nlp.jac(nlp.x0)
-    v = np.array([-1.,1.])
-    w = np.array([-2])
     print 'number of variables: ', nlp.n
     print 'initial guess: ', nlp.x0
     print 'f(x0) = ', nlp.obj(nlp.x0)
     print 'g(x0) = ', g
     print 'H(x0) = ', H
     #print 'H_sparse(x0) = ', H_sparse
-    print 'c(x0) = ', c
-    print 'J(x0) = ', J
-    print 'J(x0) * [-1,1] = ', nlp.jac_vec(nlp.x0, v)
-    print 'J(x0).T * [-2] = ', nlp.vec_jac(nlp.x0, w)
+    if nlp.m > 0 :
+        print 'number of constraints: ', nlp.m
+        c = nlp.cons(nlp.x0)
+        J = nlp.jac(nlp.x0)
+        v = np.array([-1.,-1.])
+        w = np.array([2])
+        print 'c(x0) = ', c
+        print 'J(x0) = ', J
+        print 'J(x0) * [-1,1] = ', nlp.jac_vec(nlp.x0, v)
+        print 'J(x0).T * [-2] = ', nlp.vec_jac(nlp.x0, w)
 
 #     # Solve with linesearch-based L-BFGS method.
 #     lbfgs = LBFGSFramework(nlp, npairs=5, scaling=True, silent=False)
