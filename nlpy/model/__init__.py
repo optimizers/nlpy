3ef4aafe158747c71fe8b18256e84714068f06d5
Dominique Orban <dominique.orban@gmail.com>
2011-05-11 19:05:29 -0400

    Safe imports of AD-dependent modeling tools.

--- nlpy/model/__init__.py
+++ nlpy/model/__init__.py
@@ -6,8 +6,17 @@ from nlp         import *
 from amplpy      import *
 from noisynlp    import *
 from slacks      import *
-from adolcmodel  import *
-from cppadmodel  import *
-from algopymodel import *
+try:
+    from adolcmodel  import *
+except:
+    pass
+try:
+    from cppadmodel  import *
+except:
+    pass
+try:
+    from algopymodel import *
+except:
+    pass
 
 __all__ = filter(lambda s:not s.startswith('_'), dir())
