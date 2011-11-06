a088f3307b6509cd0a20c17519fdd09788760266
Dominique Orban <dominique.orban@gmail.com>
2011-11-06 17:19:33 -0500

    Use different theme for documentation.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -118,7 +118,8 @@ todo_include_todos = True
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'nlpy.css'
+#html_style = 'nlpy.css'
+html_theme = "agogo"
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
