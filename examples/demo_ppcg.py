c37fd65cf91710f3bdd22b80cce99acd24ac8aab
Dominique Orban <dominique.orban@gmail.com>
2012-05-31 10:58:07 -0600

    Convert PPCG to use a QPModel.

--- examples/demo_ppcg.py
+++ examples/demo_ppcg.py
@@ -1,8 +1,9 @@
-from nlpy.model.amplpy import AmplModel
-from nlpy.krylov.linop import SimpleLinearOperator
-from nlpy.krylov.ppcg import ProjectedCG as Ppcg
+from nlpy.model import AmplModel, QPModel
+from nlpy.krylov import SimpleLinearOperator
+from nlpy.krylov import ProjectedCG as Ppcg
 from nlpy.tools import norms
 import numpy
+import logging
 import sys
 
 if len(sys.argv) < 2:
@@ -20,13 +21,16 @@ problem = sys.argv[1]
 nlp = AmplModel(problem)
 
 if nlp.m == 0:
-    sys.stderr.write( 'Problem should have general constraints\n' )
+    sys.stderr.write('Problem should have general constraints\n')
     nlp.close()
     sys.exit(1)
 
 x0 = numpy.ones(nlp.n) #nlp.x0            # Initial point
 g  = numpy.random.random(nlp.n) #nlp.grad(x0)      # Objective gradient
 J  = nlp.jac(x0)       # Obtain constraints Jacobian
+H  = SimpleLinearOperator(nlp.n, nlp.n,
+                          lambda p: nlp.hprod(nlp.x0,nlp.pi0,p),
+                          symmetric=True)
 delta = 1000 #max(1.0, 100 * norms.norm_infty(g))
 
 # You can test constraints with a nonzero rhs by uncommenting the following,
@@ -36,29 +40,42 @@ delta = 1000 #max(1.0, 100 * norms.norm_infty(g))
 #c = numpy.empty(nlp.m, 'd')
 #J.matvec(e, c)
 
+# Create root logger.
+rootlog = logging.getLogger('PPCG')
+rootlog.setLevel(logging.INFO)
+fmt = logging.Formatter('%(name)-10s %(levelname)-8s %(message)s')
+hndlr = logging.StreamHandler(sys.stdout)
+hndlr.setFormatter(fmt)
+rootlog.addHandler(hndlr)
+
+# Create solver logger.
+log = logging.getLogger('PPCG.ppcg')
+log.setLevel(logging.DEBUG)
+fmt = logging.Formatter('%(name)-10s %(levelname)-8s %(message)s')
+hndlr = logging.StreamHandler(sys.stdout)
+hndlr.setFormatter(fmt)
+log.addHandler(hndlr)
+
 # Call projected CG to solve problem
 #   min  <g,d> + 1/2 <d, Hd>
 #   s.t  Jd = 0,  |d| <= delta
-CG = Ppcg(g,
-          SimpleLinearOperator(nlp.n, nlp.n,
-                               lambda p: nlp.hprod(nlp.x0,nlp.pi0,p),
-                               symmetric=True),
-          A=J, radius=delta, debug=True)
+qp = QPModel(g, H, A=J)
+CG = Ppcg(qp, radius=delta, logger_name='PPCG.ppcg')
 #CG = Ppcg(g, A=J, rhs=-c, matvec=hprod, debug=True)
 CG.Solve()
 
 Jd = numpy.empty(nlp.m, 'd')
 J.matvec(CG.x, Jd)
 feas = norms.norm_infty(Jd)
-#feas = norms.norm_infty( c + Jd )  # for nonzero rhs constraint
+#feas = norms.norm_infty(c + Jd)  # for nonzero rhs constraint
 snorm = norms.norm2(CG.x) # The trust-region is in the l2 norm
 
-sys.stdout.write( 'Number of variables      : %-d\n' % nlp.n )
-sys.stdout.write( 'Number of constraints    : %-d\n' % nlp.m )
-sys.stdout.write( 'Converged                : %-s\n' % repr(CG.converged) )
-sys.stdout.write( 'Trust-region radius      : %8.1e\n' % delta )
-sys.stdout.write( 'Solution norm            : %8.1e\n' % snorm )
-sys.stdout.write( 'Final/Initial Residual   : ')
-sys.stdout.write( '%8.1e / %8.1e\n' % (CG.residNorm,CG.residNorm0) )
-sys.stdout.write( 'Feasibility error        : %8.1e\n' % feas )
-sys.stdout.write( 'Number of iterations     : %-d\n' % CG.iter )
+rootlog.info('Number of variables      : %-d' % nlp.n)
+rootlog.info('Number of constraints    : %-d' % nlp.m)
+rootlog.info('Converged                : %-s' % repr(CG.converged))
+rootlog.info('Trust-region radius      : %7.1e' % delta)
+rootlog.info('Solution norm            : %7.1e' % snorm)
+rootlog.info('Final/Initial Residual   : ' + \
+             '%7.1e / %7.1e' % (CG.residNorm,CG.residNorm0))
+rootlog.info('Feasibility error        : %7.1e' % feas)
+rootlog.info('Number of iterations     : %-d' % CG.iter)
