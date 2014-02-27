f453dd650e031b18b1bce4e39b556464aa31c0c3
Dominique Orban <dominique.orban@gmail.com>
2014-02-27 12:20:28 -0800

    Reindent and fix constraint.

--- nlpy/model/tests/test_algopymodel.py
+++ nlpy/model/tests/test_algopymodel.py
@@ -1,65 +1,85 @@
 from numpy.testing import *
 import algopy
 from nlpy.model.algopymodel import AlgopyModel
-
 import numpy as np
 
 
 class AlgopyRosenbrock(AlgopyModel):
-    "The standard Rosenbrock function."
+  "The standard Rosenbrock function."
 
-    def obj(self, x, **kwargs):
-        return algopy.sum(100*(x[1:] - x[:-1]**2)**2 + (1 - x[:-1])**2)
+  def obj(self, x, **kwargs):
+    return algopy.sum(100*(x[1:] - x[:-1]**2)**2 + (1 - x[:-1])**2)
 
 
 class AlgopyHS7(AlgopyModel):
-    "Problem #7 in the Hock and Schittkowski collection."
+  "Problem #7 in the Hock and Schittkowski collection."
 
-    def obj(self, x, **kwargs):
-        return algopy.log(1 + x[0]**2) - x[1]
+  def obj(self, x, **kwargs):
+    return algopy.log(1 + x[0]**2) - x[1]
 
-    def cons(self, x, **kwargs):
-        return (1 + x[0]**2)**2 + x[1]**2 - 4
+  def cons(self, x, **kwargs):
+    c = algopy.zeros(1, dtype=x)
+    c[0] = (1 + x[0]**2)**2 + x[1]**2 - 4
+    return c
+
+
+def get_values(nlp):
+  f = nlp.obj(nlp.x0)
+  if nlp.m > 0:
+    c = nlp.cons(nlp.x0)
+    return (f, c)
+  else:
+    return f
 
 
 def get_derivatives(nlp):
-    g = nlp.grad(nlp.x0)
-    H = nlp.dense_hess(nlp.x0, nlp.x0)
-    if nlp.m > 0:
-        J = nlp.dense_jac(nlp.x0)
-        return (g, H, J)
-    else:
-        return (g, H)
+  g = nlp.grad(nlp.x0)
+  H = nlp.dense_hess(nlp.x0, nlp.x0)
+  if nlp.m > 0:
+    J = nlp.dense_jac(nlp.x0)
+    return (g, H, J)
+  else:
+    return (g, H)
 
 
 class Test_AlgopyRosenbrock(TestCase):
 
-    def setUp(self):
-        self.rosenbrock = AlgopyRosenbrock(n=5, name='Rosenbrock', x0=-np.ones(5))
+  def setUp(self):
+    self.rosenbrock = AlgopyRosenbrock(n=5, name='Rosenbrock', x0=-np.ones(5))
 
-    def test_gradient(self):
-        (g, H) = get_derivatives(self.rosenbrock)
-        expected_g = np.array([-804., -1204., -1204., -1204., -400.])
-        expected_H = np.array([[1602.,  400.,    0.,    0.,   0.],
-                               [ 400., 1802.,  400.,    0.,   0.],
-                               [   0.,  400., 1802.,  400.,   0.],
-                               [   0.,    0.,  400., 1802., 400.],
-                               [   0.,    0.,    0.,  400., 200.]])
-        assert(np.allclose(g, expected_g))
-        assert(np.allclose(H, expected_H))
+  def test_gradient(self):
+    f = get_values(self.rosenbrock)
+    expected_f = 1616.0
+    assert_almost_equal(f, expected_f)
+
+    (g, H) = get_derivatives(self.rosenbrock)
+    expected_g = np.array([-804., -1204., -1204., -1204., -400.])
+    expected_H = np.array([[1602.,  400.,    0.,    0.,   0.],
+                           [ 400., 1802.,  400.,    0.,   0.],
+                           [   0.,  400., 1802.,  400.,   0.],
+                           [   0.,    0.,  400., 1802., 400.],
+                           [   0.,    0.,    0.,  400., 200.]])
+    assert(np.allclose(g, expected_g))
+    assert(np.allclose(H, expected_H))
 
 
 class Test_AlgopyHS7(TestCase):
 
-    def setUp(self):
-        self.hs7 = AlgopyHS7(n=2, m=1, name='HS7', x0=2*np.ones(2))
+  def setUp(self):
+    self.hs7 = AlgopyHS7(n=2, m=1, name='HS7', x0=2*np.ones(2))
 
-    def test_hs7(self):
-        hs7 = self.hs7
-        (g, H, J) = get_derivatives(hs7)
-        expected_g = np.array([0.8, -1.])
-        expected_H = np.array([[-0.24, 0.], [0., 0.]])
-        expected_J = np.array([[40., 4.]])
-        assert(np.allclose(g, expected_g))
-        assert(np.allclose(H, expected_H))
-        assert(np.allclose(J, expected_J))
+  def test_hs7(self):
+    hs7 = self.hs7
+    (f, c) = get_values(hs7)
+    expected_f = -0.39056208756589972
+    expected_c = np.array([25.0])
+    assert_almost_equal(f, expected_f)
+    assert_allclose(c, expected_c)
+
+    (g, H, J) = get_derivatives(hs7)
+    expected_g = np.array([0.8, -1.])
+    expected_H = np.array([[-0.24, 0.], [0., 0.]])
+    expected_J = np.array([[40., 4.]])
+    assert(np.allclose(g, expected_g))
+    assert(np.allclose(H, expected_H))
+    assert(np.allclose(J, expected_J))
