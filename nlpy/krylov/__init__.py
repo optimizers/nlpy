fb6331b92b02c751c35f911eef2d849505ce5fa4
Dominique Orban <dominique.orban@gmail.com>
2014-02-27 12:40:59 -0800

    Phase out Minres.

--- nlpy/krylov/__init__.py
+++ nlpy/krylov/__init__.py
@@ -4,7 +4,6 @@ Krylov Methods for Optimization
 
 from linop      import *
 from pcg        import *
-from minres     import *
 from projKrylov import *
 from ppcg       import *
 from pbcgstab   import *
