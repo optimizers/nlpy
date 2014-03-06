f6aeca2e2dd76d9e73a7033afabc5fbd2ffd7e18
Dominique Orban <dominique.orban@gmail.com>
2014-03-06 09:22:07 -0800

    New style class.

--- nlpy/model/algopymodel.py
+++ nlpy/model/algopymodel.py
@@ -10,7 +10,7 @@ class AlgopyModel(NLPModel):
     """
 
     def __init__(self, n=0, m=0, name='Algopy-Generic', **kwargs):
-        NLPModel.__init__(self, n, m, name, **kwargs)
+        super(AlgopyModel, self).__init__(n, m, name, **kwargs)
 
         try:
             self._trace_obj(self.x0)
@@ -68,74 +68,3 @@ class AlgopyModel(NLPModel):
     def dense_jac(self, x, **kwargs):
         "Return constraints Jacobian at x in dense format."
         return self._cg_cons.jacobian(x)
-
-
-
-
-# if __name__ == '__main__':
-
-#     from nlpy.optimize.solvers.lbfgs import LBFGSFramework
-#     from nlpy.optimize.solvers.ldfp  import LDFPTrunkFramework
-#     from nlpy.optimize.solvers.trunk import TrunkFramework
-#     from nlpy.optimize.tr.trustregion import TrustRegionFramework as TR
-#     from nlpy.optimize.tr.trustregion import TrustRegionCG as TRSolver
-#     import nlpy.tools.logs
-#     import logging, sys
-
-#     class AlgopyRosenbrock(AlgopyModel):
-
-#         def obj(self, x, **kwargs):
-#             return np.sum( 100*(x[1:] - x[:-1]**2)**2 + (1 - x[:-1])**2 )
-
-
-#     nvar = 10
-#     nlp = AlgopyRosenbrock(n=nvar, name='Rosenbrock', x0=-np.ones(nvar))
-
-
-#     class hs7(AlgopyModel):
-
-#         def obj(self, x, **kwargs):
-#             return np.log(1 + x[0]**2) - x[1]
-
-#         def cons(self, x, **kwargs):
-#             return (1 + x[0]**2)**2 + x[1]**2 - 4
-
-#     g = nlp.grad(nlp.x0)
-#     # print 'initial guess: ', nlp.x0
-#     # print 'number of variables: ', nlp.n
-#     # print 'f(x0) = ', nlp.obj(nlp.x0)
-#     # print 'g(x0) = ', g
-
-#     # Solve with linesearch-based L-BFGS method.
-#     lbfgs = LBFGSFramework(nlp, npairs=5, scaling=True, silent=False)
-#     lbfgs.solve()
-
-#     # Create root logger.
-#     log = logging.getLogger('adolcmodel')
-#     log.setLevel(logging.INFO)
-#     fmt = logging.Formatter('%(name)-15s %(levelname)-8s %(message)s')
-#     hndlr = logging.StreamHandler(sys.stdout)
-#     hndlr.setFormatter(fmt)
-#     log.addHandler(hndlr)
-
-#     # Configure the subproblem solver logger
-#     nlpy.tools.logs.config_logger('adolcmodel.ldfp',
-#                                   filemode='w',
-#                                   stream=sys.stdout)
-
-#     tr = TR(Delta=1.0, eta1=0.05, eta2=0.9, gamma1=0.25, gamma2=2.5)
-
-#     # Solve with trust-region-based L-DFP method.
-# #    ldfp = LDFPTrunkFramework(nlp, tr, TRSolver,
-# #                              ny=True, monotone=False,
-# #                              logger_name='adolcmodel.ldfp')
-# #    ldfp.TR.Delta = 0.1 * np.linalg.norm(g)         # Reset initial trust-region radius
-# #    ldfp.Solve()
-
-#     # Solve with trust-region-based method.
-#     trnk = TrunkFramework(nlp, tr, TRSolver,
-#                           ny=True, monotone=False,
-#                           logger_name='adolcmodel.ldfp')
-#     trnk.TR.Delta = 0.1 * np.linalg.norm(g)         # Reset initial trust-region radius
-#     trnk.Solve()
-
