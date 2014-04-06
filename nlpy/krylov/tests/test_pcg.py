bcfdfe510a6b0961721b59e61ffded9bf3279dc2
Dominique Orban <dominique.orban@gmail.com>
2014-04-05 23:14:46 -0700

    Update pcg tests.

--- nlpy/krylov/tests/test_pcg.py
+++ nlpy/krylov/tests/test_pcg.py
@@ -3,7 +3,7 @@ from numpy.testing import *
 import unittest
 from nlpy.model import QPModel
 from nlpy.krylov import TruncatedCG
-from nlpy.krylov import SimpleLinearOperator
+from pykrylov.linop import LinearOperator
 import numpy as np
 
 
@@ -20,7 +20,7 @@ class Test_PCG(unittest.TestCase):
             Hx[:-1] += sub*x[:-1]
             return Hx
 
-        self.H = SimpleLinearOperator(self.n, self.n, hprod, symmetric=True)
+        self.H = LinearOperator(self.n, self.n, hprod, symmetric=True)
 
     def test_noregion(self):
         # Convex model.
@@ -29,7 +29,7 @@ class Test_PCG(unittest.TestCase):
         c = -(self.H * expected_x)
         qp = QPModel(c, self.H, A=None)
         trcg = TruncatedCG(qp)
-        trcg.Solve(radius=None)
+        trcg.solve(radius=None)
         assert(np.allclose(trcg.step, expected_x))
 
     def test_convex_interior(self):
@@ -39,7 +39,7 @@ class Test_PCG(unittest.TestCase):
         c = -(self.H * expected_x)
         qp = QPModel(c, self.H)
         trcg = TruncatedCG(qp)
-        trcg.Solve(radius=1.2*np.linalg.norm(expected_x))
+        trcg.solve(radius=1.2 * np.linalg.norm(expected_x))
         assert(np.allclose(trcg.step, expected_x))
 
     def test_convex_boundary(self):
@@ -50,7 +50,7 @@ class Test_PCG(unittest.TestCase):
         c = -(self.H * expected_x)
         qp = QPModel(c, self.H)
         trcg = TruncatedCG(qp)
-        trcg.Solve(radius=np.linalg.norm(expected_x))
+        trcg.solve(radius=np.linalg.norm(expected_x))
         assert(np.allclose(trcg.step, expected_x))
 
     def test_convex_boundary2(self):
@@ -63,7 +63,7 @@ class Test_PCG(unittest.TestCase):
         c = -(self.H * expected_x + expected_x)  # lambda := 1.
         qp = QPModel(c, self.H)
         trcg = TruncatedCG(qp)
-        trcg.Solve(radius=np.linalg.norm(expected_x))
+        trcg.solve(radius=np.linalg.norm(expected_x))
 
         # Here we can't demand too much accuracy since we could hit the border
         # close to the expected solution.
@@ -74,15 +74,15 @@ class Test_PCG(unittest.TestCase):
         # Example from the trust-region book, Section 7.3.1.2 page 179.
 
         diag = np.array([-2, -1, 0, 1], dtype=np.float)
-        H = SimpleLinearOperator(4, 4, lambda x: diag*x, symmetric=True)
+        H = LinearOperator(4, 4, lambda x: diag*x, symmetric=True)
         c = np.ones(4)
 
         # -c is a direction of negative curvature.
         radius = 2
-        expected_x = -c/np.linalg.norm(c) * radius
+        expected_x = -c / np.linalg.norm(c) * radius
         qp = QPModel(c, H)
         trcg = TruncatedCG(qp)
-        trcg.Solve(radius=np.linalg.norm(expected_x))
+        trcg.solve(radius=np.linalg.norm(expected_x))
 
 
 if __name__ == '__main__':
