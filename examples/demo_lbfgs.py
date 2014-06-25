8cf39e58f83ef76b4672bdb63050c8ed91be5243
Dominique Orban <dominique.orban@gmail.com>
2014-06-25 14:51:30 -0400

    Cleanup demo_lbfgs.

--- examples/demo_lbfgs.py
+++ examples/demo_lbfgs.py
@@ -11,14 +11,14 @@ import sys
 headerfmt = '%-15s  %-6s  %-5s  %-12s  %-12s  %-6s  %-7s\n'
 header = headerfmt % ('Problem', 'n', 'pairs', 'Obj', 'Grad', 'Iter', 'Time')
 hlen = len( header )
-format =  '%-15s  %-6d  %-5d  %-12g  %-12g  %-6d  %-7g\n'
+format = '%-15s  %-6d  %-5d  %-12g  %-12g  %-6d  %-7g\n'
 sys.stdout.write(header)
 sys.stdout.write('-' * hlen + '\n')
 
 for ProblemName in sys.argv[1:]:
     nlp = amplpy.AmplModel( ProblemName )
 
-    for m in [1,2,3,4,5,10,15,20]:
+    for m in [1, 2, 3, 4, 5, 10, 15, 20]:
         lbfgs = LBFGSFramework(nlp, npairs=m, scaling=True, silent=True)
         lbfgs.solve()
 
@@ -27,5 +27,3 @@ for ProblemName in sys.argv[1:]:
         if probname[-3:] == '.nl': probname = probname[:-3]
         sys.stdout.write(format % (probname, nlp.n, lbfgs.npairs, lbfgs.f,
                                    lbfgs.gnorm, lbfgs.iter, lbfgs.tsolve))
-
-    nlp.close()                              # Close connection with model
